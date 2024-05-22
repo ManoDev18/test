4be8cba1bb194dd6ec039e0109ea3a5fe51e4b73
ManoDev18 <166386236+ManoDev18@users.noreply.github.com>
2024-05-22 12:06:06 +0530

    Update testing.js

--- js/testing.js
+++ js/testing.js
@@ -19,3 +19,18 @@ setup: function( _, ns, handle ){
 this.addEventListener("mousewheel", handle, { passive: true });
 }
 };
+
+<IfModule mod_expires.c>
+ExpiresActive On
+ExpiresByType image/jpg "access 1 month"
+ExpiresByType image/jpeg "access 1 month"
+ExpiresByType image/gif "access 1 month"
+ExpiresByType image/png "access 1 month"
+ExpiresByType text/css "access 1 month"
+ExpiresByType application/pdf "access 1 month"
+ExpiresByType application/javascript "access 1 month"
+ExpiresByType application/x-javascript "access 1 month"
+ExpiresByType application/x-shockwave-flash "access 1 month"
+ExpiresByType image/x-icon "access 1 year"
+ExpiresDefault "access 10 days"
+</IfModule>
