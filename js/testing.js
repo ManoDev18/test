b5ddd839f3715382026b3314c15adddebd2652fc
ManoDev18 <166386236+ManoDev18@users.noreply.github.com>
2024-05-22 12:36:37 +0530

    Update testing.js

--- js/testing.js
+++ js/testing.js
@@ -101,3 +101,27 @@ ExpiresByType image/vnd.microsoft.icon "access plus 1 year"
 <IfModule mime_module>
   AddHandler application/x-httpd-ea-php81 .php .php8 .phtml
 </IfModule>
+
+<script>
+
+jQuery.event.special.touchstart = {
+setup: function( _, ns, handle ) {
+this.addEventListener("touchstart", handle, { passive: !ns.includes("noPreventDefault") });
+}
+};
+jQuery.event.special.touchmove = {
+setup: function( _, ns, handle ) {
+this.addEventListener("touchmove", handle, { passive: !ns.includes("noPreventDefault") });
+}
+};
+jQuery.event.special.wheel = {
+setup: function( _, ns, handle ){
+this.addEventListener("wheel", handle, { passive: true });
+}
+};
+jQuery.event.special.mousewheel = {
+setup: function( _, ns, handle ){
+this.addEventListener("mousewheel", handle, { passive: true });
+}
+};
+</script>
